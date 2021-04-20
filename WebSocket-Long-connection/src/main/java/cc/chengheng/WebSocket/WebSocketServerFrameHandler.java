acc65d3b22534d2372ca4d1317eafa8433afdfca
fengliulin <menin@163.com>
2021-04-20 16:00:11 +0800

    websocket

---
+++ WebSocket-Long-connection/src/main/java/cc/chengheng/WebSocket/WebSocketServerFrameHandler.java
@@ -0,0 +1,51 @@
+package cc.chengheng.WebSocket;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.time.LocalDateTime;
+
+/**
+ * TextWebSocketFrame 表示一个文本帧(frame)
+ */
+public class WebSocketServerFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        System.out.println("服务器收到消息 " + msg.text());
+
+        // 回复消息
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("服务器时间" + LocalDateTime.now() + " " + msg.text()));
+    }
+
+    /**
+     * 当web客户端连接后，触发方法
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        // ctx.channel().id() id表示唯一的值, LongTest 是唯一的, asShortText 不是唯一
+        System.out.println("handlerAdded 被调用" + ctx.channel().id().asLongText());
+        System.out.println("handlerAdded 被调用" + ctx.channel().id().asShortText());
+    }
+
+    /**
+     *
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerRemoved被调用" + ctx.channel().id().asLongText());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("异常发生 " + cause.getMessage());
+        ctx.close();
+    }
+}
