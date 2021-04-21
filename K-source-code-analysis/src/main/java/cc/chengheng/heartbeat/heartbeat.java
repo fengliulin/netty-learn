8de758369f9a46e7e563e7cd591b6433cbd7e26c
fengliulin <menin@163.com>
2021-04-21 19:27:53 +0800

    心跳源码

---
+++ K-source-code-analysis/src/main/java/cc/chengheng/heartbeat/heartbeat.java
@@ -0,0 +1,30 @@
+package cc.chengheng.heartbeat;
+
+import java.math.BigDecimal;
+
+public class heartbeat {
+    /** 贴现及买断式转贴现 */
+    private BigDecimal discountAttainBuyoutTurnDiscount;
+
+    private BigDecimal 贴现及买断式转贴现;
+
+    private BigDecimal get贴现及买断式转贴现() {
+        return 贴现及买断式转贴现;
+    }
+
+
+    /**
+     * 贴现 discount 买断Buyout   turn转   attain及。     合成。
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+
+
+
+        System.out.println(System.nanoTime()); // 纳秒就是 10亿分之1
+        Thread.sleep(1000);
+        System.out.println(System.nanoTime());
+
+    }
+}
